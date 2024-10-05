1f14fce69330f2727949f839752c177dfcf7a1c4
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-05 13:13:23 -0400

    update

--- proton-privoxy.sh
+++ proton-privoxy.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-. ./config.cfg
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+
+# Absolute path this script is in. /home/user/bin
+SCRIPT_PATH=`dirname $SCRIPT`
+
+. ${SCRIPT_PATH}/config.cfg
 
 echo ""
 echo "Installing proton-privoxy"
